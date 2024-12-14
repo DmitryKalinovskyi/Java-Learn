c2a4440fd6858d22eb38b964a7923a90d294fdfe
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-12-14 13:17:16 +0200

    Initial Lab14 commit

---
+++ Lab14/src/main/java/com/education/ztu/spring/service/TodoService.java
@@ -0,0 +1,29 @@
+package com.education.ztu.spring.service;
+
+import com.education.ztu.spring.entity.TodoItem;
+import com.education.ztu.spring.repository.TodoItemRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Component
+public class TodoService {
+
+    private final TodoItemRepository todoItemRepository;
+
+    public TodoService(TodoItemRepository todoItemRepository) {
+        this.todoItemRepository = todoItemRepository;
+    }
+
+    public List<TodoItem> getAllTodoItems() {
+        Iterable<TodoItem> itemsIterator = todoItemRepository.findAll();
+        return StreamSupport.stream(itemsIterator.spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+    public void saveTodoItem(TodoItem todoItem) {
+        todoItemRepository.save(todoItem);
+    }
+}
